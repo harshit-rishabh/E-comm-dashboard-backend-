4be7310282e633875d056b5a7f8d1146e1ef40e1
Harshit <rishabhchemistry3@gmail.com>
2023-07-24 23:27:48 +0530

    added PORT

--- database/config.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-dotenv.config();
-const url = `${process.env.REACT_APP_ATLAS_URL}`;
-const connectionparams = {
-    useNewURLParser:true,
-    useUnifiedTopology:true
-}
-mongoose.connect(url, connectionparams).then(()=>console.log('connected')).catch((err)=>console.log(err));
