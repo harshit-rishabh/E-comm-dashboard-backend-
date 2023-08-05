599494d3ce6f62874f0925f7cee06f7f7d88ce26
Harshit <rishabhchemistry3@gmail.com>
2023-08-05 16:15:12 +0530

    added jwt authentication  on routes

--- index.js
+++ index.js
@@ -9,18 +9,32 @@ app.use(cors());
 const dotenv = require('dotenv');
 dotenv.config();
 const PORT = process.env.REACT_APP_PORT || 4500;
-app.post('/signup', async (req, resp) => {
+const Jwt = require('jsonwebtoken');
+const jwtkey = process.env.JWT;
+
+app.post('/signup', tokenverify, async (req, resp) => {
     let usersignup = new signup(req.body);
     let result = await usersignup.save();
     result = result.toObject();
     delete result.pass;
-    resp.send(result);
+    Jwt.sign({result}, jwtkey, {expiresIn:'30min'}, (err, token)=>{
+        if(err){
+            resp.send({result: 'connextion is not valid...'})
+        }
+        resp.send({result, key: token});
+    })
 });
-app.post('/login', async (req, resp) => {
+app.post('/login', tokenverify, async (req, resp) => {
     if (req.body.email && req.body.pass) {
         let user = await signup.findOne(req.body).select('-pass');
         if (user) {
-            resp.send(user);
+            Jwt.sign({user}, jwtkey, {expiresIn:'30min'}, (err, token)=>{
+                if(err){
+                    resp.send({result: 'connextion is not valid...'})
+                }
+                resp.send({user, key: token});
+            })
+            
         } else {
             resp.send({ result: "Invalid User..." })
         }
@@ -30,23 +44,23 @@ app.post('/login', async (req, resp) => {
 
 })
 
-app.post('/addproduct', async (req, resp) => {
+app.post('/addproduct', tokenverify, async (req, resp) => {
     let product = new Product(req.body);
     let result = await product.save();
     resp.send(result);
 })
 
-app.get('/getlist', async (req, resp) => {
+app.get('/getlist', tokenverify, async (req, resp) => {
     let result = await Product.find();
     resp.send(result);
 })
 
-app.delete('/deleteproduct/:id', async (req, resp) => {
+app.delete('/deleteproduct/:id', tokenverify, async (req, resp) => {
     let result = await Product.deleteOne({ _id: req.params.id });
     resp.send(result)
 })
 
-app.get('/getlist/:id', async (req, resp) => {
+app.get('/getlist/:id', tokenverify, async (req, resp) => {
     let result = await Product.findOne({ _id: req.params.id });
     if (result) {
         resp.send(result);
@@ -55,7 +69,7 @@ app.get('/getlist/:id', async (req, resp) => {
     }
 });
 
-app.put('/getlist/:id', async (req, resp) => {
+app.put('/getlist/:id', tokenverify, async (req, resp) => {
     let result = await Product.updateOne(
         {
             _id: req.params.id
@@ -72,7 +86,7 @@ app.put('/getlist/:id', async (req, resp) => {
     }
 })
 
-app.get('/search/:key', async (req, resp)=>{
+app.get('/search/:key', tokenverify, async (req, resp)=>{
     let result  = await Product.find({
         '$or':[
             {name:{$regex : req.params.key}},
@@ -82,4 +96,21 @@ app.get('/search/:key', async (req, resp)=>{
     })
     resp.send(result);
 })
+
+function tokenverify(req, resp, next){
+    let token = req.headers['authorization'];
+    if(token){
+        token = token.split(' ')[1];
+        // resp.send(token);
+        Jwt.verify(token, jwtkey, (err, accepted)=>{
+            if(err){
+                resp.status(401).send({result:"plz provide valid token..."})
+            }else{
+                next();
+            }
+        })
+    }else{
+        resp.status(403).send({result:"token not found..."})
+    }
+}
 app.listen(PORT);
