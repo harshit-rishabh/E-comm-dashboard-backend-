323890f15917ff6c579f4692dbabfc0adb1dc63c
Harshit <rishabhchemistry3@gmail.com>
2023-08-05 22:34:24 +0530

    final commit

--- index.js
+++ index.js
@@ -23,38 +23,27 @@ app.post('/login', async (req, resp) => {
     if (req.body.email && req.body.pass) {
         let user = await signup.findOne(req.body).select('-pass');
         if (user) {
-            Jwt.sign({user}, jwtkey, {expiresIn:'30min'}, (err, token)=>{
-                if(err){
-                    resp.send({result: 'connection is not valid...'})
-                }
-                resp.send({user, key: token});
-            })
-            
+            resp.send(user);
         } else {
             resp.send({ result: "Invalid User..." })
         }
     } else {
         resp.send("plz enter both fields...")
     }
-
 })
-
 app.post('/addproduct', async (req, resp) => {
     let product = new Product(req.body);
     let result = await product.save();
     resp.send(result);
 })
-
 app.get('/getlist', async (req, resp) => {
     let result = await Product.find();
     resp.send(result);
 })
-
 app.delete('/deleteproduct/:id', async (req, resp) => {
     let result = await Product.deleteOne({ _id: req.params.id });
     resp.send(result)
 })
-
 app.get('/getlist/:id', async (req, resp) => {
     let result = await Product.findOne({ _id: req.params.id });
     if (result) {
@@ -63,7 +52,6 @@ app.get('/getlist/:id', async (req, resp) => {
         resp.send({ result: 'Invalid id...' });
     }
 });
-
 app.put('/getlist/:id', async (req, resp) => {
     let result = await Product.updateOne(
         {
@@ -72,7 +60,6 @@ app.put('/getlist/:id', async (req, resp) => {
         {
             $set: req.body
         }
-
     )
     if (result) {
         resp.send(result);
@@ -80,17 +67,14 @@ app.put('/getlist/:id', async (req, resp) => {
         resp.send({ result: 'Invalid id...' });
     }
 })
-
-app.get('/search/:key', async (req, resp)=>{
-    let result  = await Product.find({
-        '$or':[
-            {name:{$regex : req.params.key}},
-            {price:{$regex : req.params.key}},
-            {company:{$regex : req.params.key}}
+app.get('/search/:key', async (req, resp) => {
+    let result = await Product.find({
+        '$or': [
+            { name: { $regex: req.params.key } },
+            { price: { $regex: req.params.key } },
+            { company: { $regex: req.params.key } }
         ]
     })
     resp.send(result);
 })
-
-
 app.listen(PORT);
